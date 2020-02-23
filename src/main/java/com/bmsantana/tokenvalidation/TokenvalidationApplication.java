d5c849420c7b440565b62f708cd4c2c4c67f5bfd
Bruno Santana <brunomsantana@gmail.com>
2020-02-22 21:10:32 -0300

    estrutura basica do projeto

---
+++ src/main/java/com/bmsantana/tokenvalidation/TokenvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.bmsantana.tokenvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenvalidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmsantana/tokenvalidation/TokenvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmsantana.tokenvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenvalidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
